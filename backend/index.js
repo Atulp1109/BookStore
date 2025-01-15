bf8246e8384ce4a44e33eff5ec3d42907fdb6ddb
AtulPandey1109 <atul.pandey@neosoftmail.com>
2025-01-15 16:44:41 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const swaggerUi = require('swagger-ui-express');
 
 const swaggerDocument= require('./swagger-output.json');
 const app=express();
-const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css"
+const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.6.2/swagger-ui.min.css"
 app.use(express.json())
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, {
