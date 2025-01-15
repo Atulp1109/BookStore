679bf0ff4a8330fd58e6291cc80324ac9ae52b70
Atul Pandey <86558585+Atulp1109@users.noreply.github.com>
2025-01-15 18:19:19 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,12 +6,13 @@ const bookRouter =require ('./routes/BookRoutes.js');
 const swaggerUi = require('swagger-ui-express');
 
 const swaggerDocument= require('./swagger-output.json');
-const pathToSwaggerUi = require('swagger-ui-dist').absolutePath()
 const app=express();
+const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.18.2/swagger-ui.css"
 app.use(express.json())
-app.use(express.static(pathToSwaggerUi))
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, {
+    customCssUrl: CSS_URL,
+}));
 
 
 // allow all origin (*)
