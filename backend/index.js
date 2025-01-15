18943766cb1943c848f39f970aeb676bbf563097
Atul Pandey <86558585+Atulp1109@users.noreply.github.com>
2025-01-15 16:34:30 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,8 +11,6 @@ const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger
 app.use(express.json())
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, {
-    customCss:
-        '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
     customCssUrl: CSS_URL,
 }));
 
